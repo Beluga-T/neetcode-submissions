fc55298621034ac6c4d3dcec2a2f6325f2781794
Siwei Tan <103245404+Beluga-T@users.noreply.github.com>
2026-08-03 02:00:22 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map2 =  new HashMap<>();
+
+        
+        toFreqMap(map, s);
+        toFreqMap(map2, t);
+
+        return map.equals(map2);
+        
+    }
+
+    private HashMap<Character, Integer> toFreqMap (HashMap<Character, Integer> map, String s) {
+        char[] charArr = s.toCharArray();
+
+        for (char c: charArr) {
+            if (map.containsKey(c)) {
+                Integer freq = map.get(c);
+                freq++;
+                map.put (c, freq);
+            }
+            else {
+                map.put(c, 1);
+            }
+        }
+        return map;
+    }
+}
